75c3d00ed01252a4dea1f0b571233c1f87172426
Snowdome <kklau@outlook.com>
2020-04-30 22:13:49 +0800

    s200430
    Release version 200430

--- js/collection.js
+++ js/collection.js
@@ -17,6 +17,8 @@ function refreshData(name) {
 }
 
 function generate() {
+	p = 0;
+	
 	// Reset existing element, if any
 	document.getElementById("tableOutput").style.display = "table";
 	var nodeBr = document.querySelectorAll("table#tableOutput br").forEach(e => e.parentNode.removeChild(e));
@@ -37,9 +39,9 @@ function generate() {
 		}
 		for (i = 0; i < charNodeList.length; i++) {
 			var rareType = document.querySelector('input[name =' + CSS.escape(charNodeList[i].id + "_rank") + ']:checked').value; // Returns 3, 4, 5, AS, Both
-			var LS_value = document.getElementById(charNodeList[i].id + "_LS").value; // Returns value between 0 and 255
-			var LS_type = document.getElementById(charNodeList[i].id).className; // Returns Light or Shadow
-			csv.innerHTML += '&#13;&#10' + charNodeList[i].id + ',' + LS_value + ',' + rareType;
+			var LS_value = document.getElementById(charNodeList[i].id + "_LS").value;	// Returns value between 0 and 255
+			var LS_type = document.getElementById(charNodeList[i].id).className;	// Returns Light or Shadow
+			csv.value += "\n" + charNodeList[i].id + ',' + LS_value + ',' + rareType;	// Push value to csv Textarea
 			switch (rareType) {
 				case "Both":
 					document.getElementById("5-star").innerHTML += '<div class="container"><img src="img/' + charNodeList[i].id + '.jpg"><div class="output' + LS_type + '">' + LS_value + '</div></div>';
@@ -51,17 +53,19 @@ function generate() {
 				default:
 					document.getElementById(rank + "-star").innerHTML += '<div class="container"><img src="img/' + charNodeList[i].id + '.jpg"><div class="output' + LS_type + '">' + LS_value + '</div></div>';
 			}
+			p++;
 		}
 	}
 	
 	scroll(0,0);
-	// For 1st row in CSV field
-	csv.innerHTML = 'Id,LS_value,rareType';
+	// For 1st row in CSV Textarea
+	csv.value = 'Id,LS_value,rareType';
 	copyImg(5);
 	copyImg(4);
 	copyImg(3);
 	
 	document.getElementById("tableTitle").innerHTML = document.getElementById("title").value;
+	document.getElementById("tableTitle").innerHTML += " (" + p + ")";
 	html2canvas(document.getElementById("tableOutput")).then(function(canvas) {
 	// html2canvas(document.getElementById("tableOutput"), {y: tableOutput.getBoundingClientRect().y}).then(function(canvas) {
 		document.getElementById("canvasOutput").appendChild(canvas);
@@ -71,14 +75,79 @@ function generate() {
 	});
 	document.getElementById("charList").style.display = "none";
 	document.getElementById("generate").style.display = "none";
-	document.getElementById("reset").style.display = "inline-block";
+	document.getElementById("revise").style.display = "inline-block";
 	document.getElementById("tableOutput").style.display = "none";
 }
 
 function revise() {
 	document.getElementById("charList").style.display = "table";
 	document.getElementById("generate").style.display = "inline-block";
-	document.getElementById("reset").style.display = "none";
+	document.getElementById("revise").style.display = "none";
 	document.getElementsByTagName("canvas")[0].remove();
-	console.log("charList display style has been undone.")
+	console.log("charList display style has been undone.");
+}
+
+function importCSV() {
+	var lines = $('#csv').val().split(/\n/);
+	var texts = [];
+	for (i = 0; i < lines.length; i++) {
+		// only push this line if it contains a non whitespace character.
+		if (/\S/.test(lines[i])) {
+			texts.push($.trim(lines[i]));
+			var array = texts[i].split(',');
+			if (i > 0) {	// Skip header line
+				
+				// Check validity and set LS value
+				var id_LS = document.getElementById(array[0] + "_LS");
+				if (id_LS != null && array[1] < 256 && array[1] > -1) {
+					id_LS.value = array[1];
+				} else {
+					alert("Given data is corrupted: {"+ texts[i] + "} - invalid LS value");
+				}
+				
+				// Checck validity and set rareType
+				var id_rareType = document.getElementsByName(array[0] + "_rank");
+				if (!["0", "3", "4", "5", "AS", "Both"].includes(array[2])) {
+					alert("Given data is corrupted: {"+ texts[i] + "} - invalid rank");
+				}
+				for (options = 0; options < id_rareType.length; options++) {
+					if (id_rareType[options].value == array[2]) {
+						if (id_rareType[options].disabled == false) {
+							id_rareType[options].checked = true;
+						} else {
+							alert("Given data is corrupted: {"+ texts[i] + "} - unavailable rank");
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+function downloadCSV() {
+	var universalBOM = "\uFEFF";
+	// The download function takes a CSV string, the filename and mimeType as parameters
+	// Scroll/look down at the bottom of this snippet to see how download is called
+	var download = function(content, fileName, mimeType) {
+		var a = document.createElement('a');
+		mimeType = mimeType || 'application/octet-stream';
+
+		if (navigator.msSaveBlob) { // IE10
+			navigator.msSaveBlob(new Blob([content], {
+				type: mimeType
+			}), fileName);
+		} else if (URL && 'download' in a) { //html5 A[download]
+			a.href = URL.createObjectURL(new Blob([content], {
+				type: mimeType
+			}));
+			a.setAttribute('download', fileName);
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+		} else {
+			location.href = 'data:application/octet-stream,' + encodeURIComponent(content); // only this mime type is supported
+		}
+	}
+
+	download(universalBOM + $('#csv').val(), 'collection.csv', 'text/csv;encoding:utf-8');
 }
